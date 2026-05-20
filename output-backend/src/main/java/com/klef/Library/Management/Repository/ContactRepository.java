a1003b78e4e34a32f1074b1c2bd4495e5534ba2e
Hemanth Sai <namepallihemanthsai@gmail.com>
2026-05-20 12:08:20 +0530

    Delete output-backend directory

--- output-backend/src/main/java/com/klef/Library/Management/Repository/ContactRepository.java
+++
@@ -1,9 +0,0 @@
-package com.klef.Library.Management.Repository;
-
-import com.klef.Library.Management.Modelclass.ContactModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ContactRepository extends JpaRepository<ContactModel, String> {
-}
